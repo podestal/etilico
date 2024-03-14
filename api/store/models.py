1a7eb52572dfee32300c0df023c5a92fa00e7670
podestal <luisrodriguezpodesta@gmail.com>
2024-03-13 20:17:00 -0400

    store models done

--- api/store/models.py
+++ api/store/models.py
@@ -1,3 +1,82 @@
 from django.db import models
+from django.conf import settings
+from uuid import uuid4
 
-# Create your models here.
+class Promotion(models.Model):
+
+    description = models.CharField(max_length=255) 
+    discount = models.FloatField()
+
+    def __str__(self):
+        return self.description
+
+class Category(models.Model):
+
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products')
+    promotions = models.ManyToManyField(Promotion, blank=True )
+
+    def __str__(self):
+        return self.title
+    
+class Customer(models.Model):
+
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=255)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+class Cart(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
+    quantity = models.PositiveSmallIntegerField()
+
+class Order(models.Model):
+
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+class OrderItem(models.Model):
+
+    order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='orderitems')
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
