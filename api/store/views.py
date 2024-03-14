50c92f29b3f239ea2b7a8a6acff4eef0018b8b43
podestal <luisrodriguezpodesta@gmail.com>
2024-03-14 13:45:14 -0400

    customer me endpoing

--- api/store/views.py
+++ api/store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.decorators import action, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from django_filters.rest_framework import DjangoFilterBackend
 from . import models
@@ -53,6 +55,21 @@ class CustomerViewSet(ModelViewSet):
 
     queryset = models.Customer.objects.all()
     serializer_class = serializers.GetCustomerSerializer
+    permission_classes = [IsAdminUser]
+
+    @action(detail=False, methods=['GET', 'PUT'], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        customer = models.Customer.objects.get(user_id=request.user.id)
+        if request.method == 'GET':
+            serializer = serializers.GetCustomerSerializer(customer)
+            return Response(serializer.data)
+        elif request.method == 'PUT':
+            serializer = serializers.GetCustomerSerializer(customer, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+
+
 
 class CartViewSet(ModelViewSet): 
 
